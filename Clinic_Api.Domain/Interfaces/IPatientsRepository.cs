96e441f6c97e672391923c32b8a35ee6e45768db
Kacper <kacper.tylec1999@gmail.com>
2024-03-25 19:57:17 +0100

    Patients repository interface

---
+++ Clinic_Api.Domain/Interfaces/IPatientsRepository.cs
@@ -0,0 +1,24 @@
+﻿using Clinic_Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Api.Domain.Interfaces
+{
+    public interface IPatientsRepository
+    {
+        public Task<List<Patient>> GetPaginatedPatients(PaginationParameters pagination);
+
+        public Task<Patient> GetPatientById(string id);
+
+        public Task DeletePatient(Patient patient);
+
+        public Task UpdatePatient(Patient patient);
+
+        public Task CreatePatient(Patient patient);
+
+
+    }
+}

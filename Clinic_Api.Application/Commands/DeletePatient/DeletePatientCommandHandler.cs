2c547defc15427ae303ce7d22f23dd9b3f77a11c
Kacper <kacper.tylec1999@gmail.com>
2024-03-25 20:30:00 +0100

    DeletePatientCommandHandler

--- Clinic_Api.Application/Commands/DeletePatient/DeletePatientCommandHandler.cs
+++ Clinic_Api.Application/Commands/DeletePatient/DeletePatientCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Clinic_Api.Application.Exceptions;
+using Clinic_Api.Domain.Interfaces;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,40 @@ using System.Threading.Tasks;
 
 namespace Clinic_Api.Application.Commands.DeletePatient
 {
-    internal class DeletePatientCommandHandler
+    public class DeletePatientCommandHandler : IRequestHandler<DeletePatientCommandRequest>
     {
+        private readonly IPatientsRepository _patientsRepository;
+
+        public DeletePatientCommandHandler(IPatientsRepository patientsRepository)
+        {
+            _patientsRepository = patientsRepository;
+        }
+
+        public async Task Handle(DeletePatientCommandRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var patient = await _patientsRepository.GetPatientById(request.Id);
+                if (patient == null)
+                {
+                    throw new NotFoundException("Can`t find patient with specified Id");
+                }
+                
+                await _patientsRepository.DeletePatient(request.Id);
+
+                return;
+            }
+            catch (NotFoundException ex)
+            {
+
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
     }
 }
--- Clinic_Api.Application/Commands/DeletePatient/DeletePatientCommandRequest.cs
+++ Clinic_Api.Application/Commands/DeletePatient/DeletePatientCommandRequest.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Clinic_Api.Application.Commands.DeletePatient
 {
-    public class DeletePatientCommandRequest(string id): IRequest
+    public record DeletePatientCommandRequest(string Id): IRequest
     {
     }
 }

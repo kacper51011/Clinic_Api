57f9cf2492f120588e33d923dc261a004169e032
Kacper <kacper.tylec1999@gmail.com>
2024-03-26 21:14:44 +0100

    GetPatient metadata, docker

---
+++ Clinic_Api.Application/Queries/GetPatientMetadata/GetPatientMetadataQuery.cs
@@ -0,0 +1,14 @@
+﻿using Clinic_Api.Application.Dto;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Api.Application.Queries.GetPatientMetadata
+{
+    public record GetPatientMetadataQuery(string Id): IRequest<GetPatientMetadataResponseDto>
+    {
+    }
+}
---
+++ Clinic_Api.Application/Queries/GetPatientMetadata/GetPatientMetadataQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using Clinic_Api.Application.Dto;
+using Clinic_Api.Application.Exceptions;
+using Clinic_Api.Domain.Interfaces;
+using MediatR;
+
+namespace Clinic_Api.Application.Queries.GetPatientMetadata
+{
+    public class GetPatientMetadataQueryHandler : IRequestHandler<GetPatientMetadataQuery, GetPatientMetadataResponseDto>
+    {
+        private readonly IPatientsRepository _patientsRepository;
+
+        public GetPatientMetadataQueryHandler(IPatientsRepository patientsRepository)
+        {
+            _patientsRepository = patientsRepository;
+        }
+
+        public async Task<GetPatientMetadataResponseDto> Handle(GetPatientMetadataQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var patient = await _patientsRepository.GetPatientById(request.Id);
+                if (patient == null)
+                {
+                    throw new NotFoundException("Couldn`t find patient with specified Id");
+                }
+                var data = new GetPatientMetadataResponseDto()
+                {
+                    PatientId = patient.PatientId,
+                    CreatedAt = patient.CreatedAt,
+                    UpdatedAt = patient.UpdatedAt,
+                    Version = patient.Version,
+
+
+                };
+
+                return data;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
